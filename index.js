26f7f0a11b5ece8ff5d70fadba8a8050e941397e
Murewa Ajala <murewatajala@gmail.com>
2024-12-08 21:13:28 +0100

    New branch for chatbot impl

--- index.js
+++ index.js
@@ -156,5 +156,5 @@ app.post('/send-emails', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 }); 

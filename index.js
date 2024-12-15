dfb17373b51f2c4ab299fff2769b833d906602bb
Murewa Ajala <murewatajala@gmail.com>
2024-12-15 23:17:52 +0100

    New

--- index.js
+++ index.js
@@ -231,5 +231,5 @@ app.post('/send-emails', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port $http://localhost:3000`);
 });
